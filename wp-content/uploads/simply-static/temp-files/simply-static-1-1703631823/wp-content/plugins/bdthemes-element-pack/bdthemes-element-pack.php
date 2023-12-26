c7c7efc0ea97b444d94e03ea247e2ee7d80e5dcf
betancourtdev <thebetancourtdev@gmail.com>
2023-12-26 18:15:56 -0500

    comingson

---
+++ wp-content/uploads/simply-static/temp-files/simply-static-1-1703631823/wp-content/plugins/bdthemes-element-pack/bdthemes-element-pack.php
@@ -0,0 +1,5 @@
+<br>
+<b>Fatal error</b>:  Uncaught Error: Call to undefined function update_option() in C:\xampp\htdocs\Project-Ayax\wp-content\plugins\bdthemes-element-pack\bdthemes-element-pack.php:17
+Stack trace:
+#0 {main}
+  thrown in <b>C:\xampp\htdocs\Project-Ayax\wp-content\plugins\bdthemes-element-pack\bdthemes-element-pack.php</b> on line <b>17</b><br>
